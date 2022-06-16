c174c492969ffd3707097e5e334480221ec68251
Samuel <samucabalena@gmail.com>
2022-06-16 02:59:13 -0300

    styles: added globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
+import { GlobalStyles } from './styles/global';
+
+const Title = styled.h1`
+  color: #8257e6;
+`
 
 export function App() {
   return (
     <div className="App">
-        <h1>Olá</h1>
+        <Title>Olá mundo</Title>
+        <GlobalStyles/>
     </div>
   );
 }
